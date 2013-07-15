ad6101c7025963f8a5d286ab9a387385d477d00f
Crawford Comeaux <crawford.comeaux@gmail.com>
2013-07-15 06:42:10 +0000

    Added URL support to grader

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLURL_DEFAULT = "https://raw.github.com/crawfordcomeaux/bitstarter/master/index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,7 +67,18 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
+    if (program.url != undefined) {
+      rest.get(program.url).on('complete', function(result) {
+        if (result instanceof Error) {
+          console.log("%s does not exist. Exiting.", inurl);
+          process.exit(1);
+        } else {
+          program.file = result;
+        }
+      });
+    }
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
